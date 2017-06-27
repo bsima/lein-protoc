3a0eb74de26c5e495b77786db9908745930782e2
Phil Hagelberg <phil@hagelb.org>
2017-06-26 17:39:59 -0700

    Don't recompile proto files when they are all up-to-date.
    Fixes #3.

--- test/leiningen/protoc_test.clj
+++ test/leiningen/protoc_test.clj
@@ -7,7 +7,24 @@
 (def project
   (lcp/read "test/test-project.clj"))
 
+(def proto-file (io/file "test/proto/Foo.proto"))
+
+(def java-file (io/file "test/target/generated-sources/protobuf/com/liaison/foo/FooProtos.java"))
+
 (t/deftest protoc-test
-  (let [result (lp/protoc project)]
-    (t/is (.exists (io/file "test/proto/Foo.proto")))
-    (t/is (.exists (io/file "test/target/generated-sources/protobuf/com/liaison/foo/FooProtos.java")))))
+  (.delete java-file)
+  (let [result (lp/protoc project)
+        first-timestamp (.lastModified java-file)]
+    (t/is (.exists proto-file))
+    (t/is (.exists java-file))
+    ;; lastModified time only has resolution down to the second, not ms
+    (Thread/sleep 1000)
+    (lp/protoc project)
+    (let [second-timestamp (.lastModified java-file)]
+      (t/is (.exists java-file))
+      (t/is (= first-timestamp second-timestamp))
+      (Thread/sleep 1000)
+      (.setLastModified proto-file (System/currentTimeMillis))
+      (lp/protoc project)
+      (t/is (.exists java-file))
+      (t/is (not= first-timestamp (.lastModified java-file))))))
