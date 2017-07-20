72501b6cdf3bdb9cb0b233cc40bf48f64571cef1
Matt Anderson <manderson@liaison.com>
2017-07-20 09:12:03 -0400

    0.3.0->0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-protoc "0.3.0"
+(defproject lein-protoc "0.4.0"
   :description "Leiningen plugin for compiling Protocol Buffers"
   :url "https://github.com/LiaisonTechnologies/lein-protoc"
   :license {:name "Eclipse Public License"
